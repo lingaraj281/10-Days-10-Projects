8f43779015922b19d60301865b9b13cb11197a82
Lingaraj Sahu <lingarajs2002@gmail.com>
2021-09-21 18:05:09 +0530

    day2 reports

--- day1.py
+++ day1.py
@@ -1,5 +1,4 @@
 #Day 1: L33t$p3@]< (LeetSpeak)
-#This is only encoding part
 import random
 leetmessage = ""
 charMapping = {'a': ['4', '@', '/-\\'], 'c': ['('], 'd': ['|)'], 'e': ['3'],
@@ -22,13 +21,27 @@ def converttoleet(user_message):
             leetmessage += char
     return leetmessage
 
+def decode(leetmessage):
+    leetdict = {'a': ['4', '@', '/-\\'], 'c': ['('], 'd': ['|)'], 'e': ['3'],
+               'f': ['ph'], 'h': [']-[', '|-|'], 'i': ['1', '!', '|'], 'k': [']<'],
+               'o': ['0'], 's': ['$', '5'], 't': ['7', '+'], 'u': ['|_|'],
+               'v': ['\\/']}
+
+    message = leetmessage
+
+    for keys in leetdict.keys():
+        for values in leetdict[keys]:
+            if values in message:
+                message = message.replace(values,keys)
+    return message
+
 
 
 a = input("Enter your message : ")
 secret_message = converttoleet(a)
+decoded_message = decode(secret_message)
 print("Your original message is {}".format(a))
 print("Your encoded message is {}".format(secret_message))
-
-
+print("Decoded message is {}".format(decoded_message))
 
 
